57004d1598d4a3090a6132d7a892910bf381c6da
Christopher J. Stehno <chris.stehno@gmail.com>
2022-05-14 10:15:33 -0500

    added junit helper

---
+++ src/main/java/io/github/cjstehno/ersatz/socket/junit/ErsatzSocketServerExtension.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2022 Christopher J. Stehno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.cjstehno.ersatz.socket.junit;
+
+import io.github.cjstehno.ersatz.socket.ErsatzSocketServer;
+import lombok.val;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.lang.reflect.Field;
+
+import static java.util.Arrays.stream;
+
+/**
+ * JUnit 5 Extension used to provide a simple means of managing an ErsatzSocketServer instance during testing.
+ * <p>
+ * A field of type <code>ErsatzSocketServer</code> must be specified in the test class.
+ * <p>
+ * BeforeEach test - the server will be started.
+ * AfterEach test - the server will be stopped and the interactions will be reset.
+ */
+public class ErsatzSocketServerExtension implements BeforeEachCallback, AfterEachCallback {
+
+    @Override public void beforeEach(ExtensionContext context) throws Exception {
+        findInstance(context.getRequiredTestInstance(), true).start();
+    }
+
+    @Override public void afterEach(final ExtensionContext context) throws Exception {
+        val instance = findInstance(context.getRequiredTestInstance(), false);
+        if (instance != null) {
+            instance.close();
+            instance.resetInteractions();
+        }
+    }
+
+    private static ErsatzSocketServer findInstance(final Object testInstance, final boolean create) throws Exception {
+        val field = findField(testInstance);
+        Object instance = field.get(testInstance);
+
+        if (instance == null && create) {
+            instance = field.getType().getDeclaredConstructor().newInstance();
+            field.set(testInstance, instance);
+        }
+
+        return (ErsatzSocketServer) instance;
+    }
+
+    private static Field findField(final Object testInstance) throws Exception {
+        val field = stream(testInstance.getClass().getDeclaredFields())
+            .filter(f -> f.getType().getSimpleName().endsWith("ErsatzSocketServer"))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("An ErsatzSocketServer field must be specified."));
+
+        field.setAccessible(true);
+        return field;
+    }
+}
--- src/test/java/io/github/cjstehno/ersatz/socket/ErsatzSocketServerTest.java
+++ src/test/java/io/github/cjstehno/ersatz/socket/ErsatzSocketServerTest.java
@@ -15,10 +15,10 @@
  */
 package io.github.cjstehno.ersatz.socket;
 
+import io.github.cjstehno.ersatz.socket.junit.ErsatzSocketServerExtension;
 import lombok.val;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -26,43 +26,38 @@ import java.io.IOException;
 import java.util.Set;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(ErsatzSocketServerExtension.class)
 class ErsatzSocketServerTest {
 
-    // FIXME: ensure can have multiple onMessage blocks
-    // FIXME: need a JUnit 5 Extension for this (call reset after each)
-
-    private ErsatzSocketServer server;
-
-    @BeforeEach void beforeEach() {
-        server = new ErsatzSocketServer(cfg -> {
-            // encode the response to the connection event
-            cfg.encoder(Integer.class, (message, stream) -> {
-                val out = new DataOutputStream(stream);
-                out.writeInt((int) message);
-                out.flush();
-            });
-
-            // decode the incoming message
-            cfg.decoder(String.class, stream -> {
-                val input = new DataInputStream(stream);
-                val length = input.readInt();
-                val bytes = input.readNBytes(length);
-                return new String(bytes, UTF_8);
-            });
-
-            // encode the outgoing response message
-            cfg.encoder(String.class, (message, stream) -> {
-                val out = new DataOutputStream(stream);
-                val bytes = ((String) message).getBytes(UTF_8);
-                out.writeInt(bytes.length);
-                out.write(bytes);
-            });
+    private ErsatzSocketServer server = new ErsatzSocketServer(cfg -> {
+        // encode the response to the connection event
+        cfg.encoder(Integer.class, (message, stream) -> {
+            val out = new DataOutputStream(stream);
+            out.writeInt((int) message);
+            out.flush();
         });
-    }
+
+        // decode the incoming message
+        cfg.decoder(String.class, stream -> {
+            val input = new DataInputStream(stream);
+            val length = input.readInt();
+            val bytes = input.readNBytes(length);
+            return new String(bytes, UTF_8);
+        });
+
+        // encode the outgoing response message
+        cfg.encoder(String.class, (message, stream) -> {
+            val out = new DataOutputStream(stream);
+            val bytes = ((String) message).getBytes(UTF_8);
+            out.writeInt(bytes.length);
+            out.write(bytes);
+        });
+    });
 
     @Test void usage() throws IOException {
         server.interactions(ix -> {
@@ -85,7 +80,47 @@ class ErsatzSocketServerTest {
         )));
     }
 
-    @AfterEach void afterEach() throws IOException {
-        server.close();
+    @Test void usageWithMultipleBlocks() throws IOException {
+        server.interactions(ix -> {
+            ix.onConnect(ctx -> {
+                ctx.send(2);
+            });
+
+            ix.onMessage(equalTo("Message-0"), (ctx, message) -> {
+                ctx.send("response-0");
+            });
+
+            ix.onMessage(equalTo("Message-1"), (ctx, message) -> {
+                ctx.send("response-1");
+            });
+        });
+
+        val client = new AlphaClient(server.getPort());
+        client.connect();
+
+        val responses = client.getResponses();
+        assertEquals(2, responses.size());
+        assertTrue(responses.containsAll(Set.of("response-0", "response-1")));
+    }
+
+    @Test void usageWithMore() throws IOException {
+        server.interactions(ix -> {
+            ix.onConnect(ctx -> {
+                ctx.send(5);
+            });
+
+            ix.onMessage(startsWith("Message-"), (ctx, message) -> {
+                ctx.send(message + "-modified");
+            });
+        });
+
+        val client = new AlphaClient(server.getPort());
+        client.connect();
+
+        val responses = client.getResponses();
+        assertEquals(5, responses.size());
+        assertTrue(responses.containsAll(Set.of(
+            "Message-0-modified", "Message-1-modified", "Message-2-modified", "Message-3-modified", "Message-4-modified"
+        )));
     }
 }
